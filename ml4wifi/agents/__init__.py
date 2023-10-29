c24bd8144befeb9b2c0e01a7ffa31b61a2c75977
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2023-10-29 10:43:39 +0100

    Agent Factory implemented (not tested)

--- ml4wifi/agents/__init__.py
+++ ml4wifi/agents/__init__.py
@@ -25,31 +25,31 @@ class MAPCAgent():
             self,
             find_groups_dict: Dict,
             assign_stations_dict: Dict,
-            ap_group_id_to_ap_group: Callable,
-            sta_group_id_to_sta_group: Callable
+            ap_group_action_to_ap_group: Callable,
+            sta_group_action_to_sta_group: Callable
         ) -> None:
 
         self.find_groups_dict = find_groups_dict
         self.assign_stations_dict = assign_stations_dict
-        self.ap_group_id_to_ap_group = ap_group_id_to_ap_group
-        self.sta_group_id_to_sta_group = sta_group_id_to_sta_group
+        self.ap_group_action_to_ap_group = ap_group_action_to_ap_group
+        self.sta_group_action_to_sta_group = sta_group_action_to_sta_group
     
+
     def sample(self, reward: Scalar, sharinng_ap: int, transmitting_station: int) -> int:
         
         # Sample the agent which groups access points
-        ap_group = self.ap_group_id_to_ap_group(
+        ap_group = self.ap_group_action_to_ap_group(
             self.find_groups_dict[transmitting_station].sample(reward=reward),
             sharinng_ap
         )
 
         # Sample the agent which assigns stations to access points groups
-        sta_group = self.sta_group_id_to_sta_group(
+        sta_group = self.sta_group_action_to_sta_group(
             self.assign_stations_dict[ap_group].sample(reward=reward),
             ap_group
         )
 
         return ap_group, sta_group
-
 
 
 class MAPCAgentFactory():
@@ -73,6 +73,7 @@ class MAPCAgentFactory():
         # Define number of groups each sharing AP can create
         self.no_groups = jnp.power(2, self.n_ap - 1)
 
+
     def hierarchical_agent(self) -> None:
         
         # Define dictionary of Agent who are selecting groups
@@ -95,8 +96,15 @@ class MAPCAgentFactory():
             ) for group in powerset(self.access_points)
         }
 
-        # TODO Create and return the MAPCAgent
+        # Define the MAPCAgent
+        return MAPCAgent(
+            find_groups_dict=find_groups,
+            assign_stations_dict=assign_stations,
+            ap_group_action_to_ap_group=self._ap_group_action_to_ap_group,
+            sta_group_action_to_sta_group=self._sta_group_action_to_sta_group
+        )
     
+
     def _assign_stations_params(self, group: List[int], agent_params: Dict) -> Dict:
 
         params = copy.deepcopy(agent_params)
@@ -107,14 +115,55 @@ class MAPCAgentFactory():
 
         return params
     
-    def _ap_group_id_to_ap_group(self, ap_group_id: int, sharing_ap: int) -> List[int]:
+
+    def _ap_group_action_to_ap_group(self, ap_group_action: int, sharing_ap: int) -> List[int]:
+        """
+        Translates the action of the agent to the list of access points which are sharing the channel.
+
+        Parameters
+        ----------
+        ap_group_action : int
+            The action of agent responsible for the selection of the access points group.
+        sharing_ap : int
+            The designated access point which has won the DCF contention and is sharing the channel.
+
+        Returns
+        -------
+        List[int]
+            The list of access points which are sharing the channel with ``sharing_ap``.
+        """
+
         ap_set = set(self.access_points).difference({sharing_ap})
-        return list(powerset(ap_set))[ap_group_id]
+        return list(powerset(ap_set))[ap_group_action]
         
 
-    def _sta_group_id_to_sta_group(self, sta_group_id: int, ap_group: List[int]) -> List[int]:
-        # TODO
-        pass
+    def _sta_group_action_to_sta_group(self, sta_group_action: int, ap_group: List[int]) -> List[int]:
+        """
+        Translates the action of the agent to the list of stations which are served simoultaneously.
+
+        Parameters
+        ----------
+        sta_group_action : int
+            The action of agent responsible for the selection of the stations group.
+        ap_group : List[int]
+            The list of access points which are sharing the same channel. These access points choose one
+            station each to transmit.
+
+        Returns
+        -------
+        List[int]
+            The list os stations which are served simoultaneously by the access points in ``ap_group``.
+        """
+        
+        sta_group_id = sta_group_action + 1
+        sta_group = []
+        divider = 1.0
+        for ap in ap_group:
+            sta_group += jnp.ceil(sta_group_id / divider) % len(self.associations[ap])
+            divider *= len(self.associations[ap])
+        
+        return sta_group
+
 
 
         
