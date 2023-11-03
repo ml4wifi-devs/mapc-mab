a049580f2dfea78b88769e9092d3e065ab5f4dc1
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2023-11-03 14:13:24 +0100

    renamed scenario alignment files

--- ml4wifi/params_fit/scenario_1_alignment.py
+++ ml4wifi/params_fit/scenario_1_alignment.py
@@ -2,8 +2,10 @@ import sys
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
 from ml4wifi.envs.scenarios.static import *
+import ml4wifi.plots
 
 def run(distance: int, mcs: int = 11, seed: int = 42, plot: bool = False):
 
@@ -74,23 +76,11 @@ def run(distance: int, mcs: int = 11, seed: int = 42, plot: bool = False):
     return jnp.mean(thr_optimal), jnp.mean(thr_suboptimal), jnp.mean(thr_wasteful)
 
 
-if __name__ == "__main__":
+def plot_cumulative():
 
-    mcs = sys.argv[1]
-    print(f"=== MCS {mcs} ===")
-
-    mean_optimal, mean_suboptimal, mean_wasteful = [], [], []
-    distances = jnp.logspace(-3, 3, 10, base=10)
-    for d in distances:
-        thr_optimal, thr_suboptimal, thr_wasteful = run(distance=d, mcs=int(mcs), plot=True)
-        mean_optimal.append(thr_optimal)
-        mean_suboptimal.append(thr_suboptimal)
-        mean_wasteful.append(thr_wasteful)
-        print(f"Distance {d:.3f}m: {thr_optimal:.2f} > {thr_suboptimal:.2f} > {thr_wasteful:.2f}")
-    
-    plt.plot(distances, mean_optimal, label='optimal')
-    plt.plot(distances, mean_suboptimal, label='suboptimal')
-    plt.plot(distances, mean_wasteful, label='wasteful')
+    plt.plot(distances, mean_optimal, label='o-AP1 o o AP2-o (optimal)')
+    plt.plot(distances, mean_suboptimal, label='o-AP1 o o-AP2 o (suboptimal)')
+    plt.plot(distances, mean_wasteful, label='o AP1-o o-AP2 o (wasteful)')
     plt.xscale('log')
     plt.xlabel('Distance [m]')
     plt.ylabel('Approximated throughput [Mb/s]')
@@ -100,4 +90,42 @@ if __name__ == "__main__":
     plt.tight_layout()
     plt.savefig(f'scenario_1_cum_mcs{mcs}.pdf', bbox_inches='tight')
     plt.clf()
+
+if __name__ == "__main__":
+
+    # Define argument parser
+    parser = ArgumentParser()
+    parser.add_argument("-m", "--mcs", type=int, help="MCS index")
+    parser.add_argument("-r", "--resolution", type=int, default=50, help="The distance space resolution to search")
+    parser.add_argument("-p", "--plot", action="store_true", help="Plot throughput in time")
+
+    # Parse arguments
+    args = parser.parse_args()
+    mcs = args.mcs
+    res = args.resolution
+    plot_flag = args.plot
+
+    mcs = sys.argv[1]
+    print(f"=== MCS {mcs} ===")
+
+    mean_optimal, mean_suboptimal, mean_wasteful = [], [], []
+    distances = jnp.logspace(-3, 3, res, base=10)
+    for d in distances:
+        thr_optimal, thr_suboptimal, thr_wasteful = run(distance=d, mcs=int(mcs), plot=True)
+        mean_optimal.append(thr_optimal)
+        mean_suboptimal.append(thr_suboptimal)
+        mean_wasteful.append(thr_wasteful)
+        print(f"Distance {d:.3f}m: {thr_optimal:.2f} > {thr_suboptimal:.2f} > {thr_wasteful:.2f}")
+    
+    # Plot the approximate throughput
+    plot_cumulative()
+
+    # Plot the scenario topology
+    d = 10
+    tmp_scenario = simple_scenario_1(d=d, mcs=mcs)
+    tmp_scenario.plot(f'scenario_2_top_mcs{mcs}_dap_{d}.pdf')
+
+# o-AP1 o o AP2-o (optimal)
+# o-AP1 o o-AP2 o (suboptimal)
+# o AP1-o o-AP2 o (wasteful)
 
