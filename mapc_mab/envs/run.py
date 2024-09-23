2de11d8841bedd3e7e72307731c9c7c947c108e4
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-09-23 19:08:55 +0200

    Add JAX compilation cache and fix seeding

--- mapc_mab/envs/run.py
+++ mapc_mab/envs/run.py
@@ -1,5 +1,8 @@
 import os
 os.environ['JAX_ENABLE_X64'] = 'True'
+os.environ['JAX_COMPILATION_CACHE_DIR'] = '/tmp/jax_cache'
+os.environ['JAX_PERSISTENT_CACHE_MIN_ENTRY_SIZE_BYTES'] = '-1'
+os.environ['JAX_PERSISTENT_CACHE_MIN_COMPILE_TIME_SECS'] = '0'
 
 import json
 from copy import deepcopy
@@ -23,13 +26,11 @@ def run_scenario(
         seed: int
 ) -> tuple[list, list]:
     key = jax.random.PRNGKey(seed)
-    agent_copy = agent_factory.create_mapc_agent()
-
     runs = []
     actions = []
 
     for _ in range(n_reps):
-        agent = deepcopy(agent_copy)
+        agent = agent_factory.create_mapc_agent()
         scenario.reset()
         runs.append([])
         actions.append([])
